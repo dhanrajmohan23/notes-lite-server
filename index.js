a8f2c3c7a1bb57e82eb88baebe13bc66ee5991b0
dhanrajMohan <dhanrajmohan23@gmail.com>
2023-06-07 07:49:28 +0530

    bug fix

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const routes = require("./routes/index");
 const mongoString = process.env.DATABASE_URL;
 
