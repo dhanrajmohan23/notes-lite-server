9044dd01d15c5fc987be9f8b886eb9c9f5f720d1
dhanrajMohan <dhanrajmohan23@gmail.com>
2023-06-07 07:21:24 +0530

    Timestamp added in schema and port updated

--- model/model.js
+++ model/model.js
@@ -1,14 +1,19 @@
 const mongoose = require("mongoose");
 
-const dataSchema = new mongoose.Schema({
-  name: {
-    required: true,
-    type: String,
+const dataSchema = new mongoose.Schema(
+  {
+    name: {
+      required: true,
+      type: String,
+    },
+    age: {
+      required: true,
+      type: Number,
+    },
   },
-  age: {
-    required: true,
-    type: Number,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Data", dataSchema);
